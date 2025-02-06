3d6f4b8e7803751080835c090d914943a2defc3e
Laurah <115381945+laudvs@users.noreply.github.com>
2025-02-05 22:34:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const CLIENT_ID = 'd83eef9d38aa439fa19b1a9898fdab09';
-const REDIRECT_URI = 'http://localhost:3000/inicio.html';
+const REDIRECT_URI = 'http://127.0.0.1:5500/playlistando-main/pages/inicio.html';
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const RESPONSE_TYPE = 'token';
 const SCOPE = 'playlist-modify-public playlist-modify-private';
 
-document.getElementById('spotify-login').addEventListener('click', () => {
-    const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=${RESPONSE_TYPE}&scope=${encodeURIComponent(SCOPE)}`;
-    window.location.href = authUrl;
+import { handleSpotifyLogin } from './auth.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('spotify-login').addEventListener('click', handleSpotifyLogin);
 });
